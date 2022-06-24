2a066b43bc953a91b569e8107dc3b3bc455114f5
souleymane <souleymane.ah@sancfis.net>
2022-06-24 11:22:05 +0100

    second commit

--- store/views/checkout.py
+++ store/views/checkout.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from store.models import *
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        custumer = request.user.customer
+        order, created = Order.objects.get_or_create(custumer, complete = False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+
+    context = {'items':items, 'order':order}
     return render(request, 'store/checkout.html', context)
